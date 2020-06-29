17fbf4a6e9690ce1ac1357c50e581e3c2d91086a
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-06-29 21:24:01 +0300

    Done!
    Done!

---
+++ Development/js/scripts.js
@@ -0,0 +1,97 @@
+//<!-- 👇🏻👇🏻التمرين الأول -->
+
+// var foods = ["Pasta", "Rice", "Sushi", "Meat"];
+
+// let ordernumber = prompt("choose number from 0 to 3");
+
+// if (ordernumber == 0) {
+//   console.log("You like Pasta");
+// }
+
+// if (ordernumber == 1) {
+//   console.log("You like Rice");
+// }
+
+// if (ordernumber == 2) {
+//   console.log("You like Sushi");
+// }
+
+// if (ordernumber == 3) {
+//   console.log("You like Meat");
+// }
+
+// switch (ordernumber) {
+//   case "0":
+//     alert("You like Pasta");
+//     break;
+//   case "1":
+//     alert("You like Rice");
+//     break;
+//   case "2":
+//     alert("You like Sushi");
+//     break;
+//   case "3":
+//     alert("You like Meat");
+//     break;
+//   default:
+//     alert("You dont like anything");
+// }
+
+//<!-- التمرين الأول -->
+
+//<!-- 👇🏻👇🏻التمرين الثاني -->
+
+// var n1 = parseInt(prompt("ادخل الرقم الاول"));
+// var n2 = parseInt(prompt("ادخل الرقم الثاني"));
+// var operations = prompt("(/,*,-,+) اختر العملية");
+
+// if (operations == null) {
+//   console.log("try again!");
+// }
+// if (operations == "+") {
+//   console.log(n1 + n2);
+// }
+// if (operations == "-") {
+//   console.log(n1 - n2);
+// }
+// if (operations == "*") {
+//   console.log(n1 * n2);
+// }
+// if (operations == "/") {
+//   console.log(n1 / n2);
+// }
+
+// switch (operations) {
+//   case "+":
+//     console.log(n1 + n2);
+//     break;
+//   case "-":
+//     console.log(n1 - n2);
+//     break;
+//   case "*":
+//     console.log(n1 * n2);
+//     break;
+//   case "/":
+//     console.log(n1 / n2);
+//     break;
+// }
+
+//<!-- التمرين الثاني -->
+
+//<!-- 👇🏻👇🏻التمرين الثالث -->
+
+age = 15;
+
+let message = prompt("How old are you ?");
+
+if (message > age) {
+  alert("جامعة");
+}
+if (message == age) {
+  alert("ثانوية");
+}
+if (message < age) {
+  alert("ابتدائي");
+}
+
+//<!-- التمرين الثالث -->
